0d3352156b4d6434c5b36157c9efb3b3e3b4fdd7
blue-harry <96937529+blue-harry@users.noreply.github.com>
2024-12-03 11:16:16 +0800

    fw

--- src/coding/motto.js
+++ src/coding/motto.js
@@ -1,13 +1,14 @@
 /**
  *  You set your style values below
  */
-const FIRST_LINE = "Slow and steady"
-const SECOND_LINE = "win the game"
+const FIRST_LINE = " I am edward."
+const SECOND_LINE = "I win the game."
 const RADIUS = 50
 const PSIZE = 3
 const WORD_SIZE = 14
-const STROKE_COLOR = "255, 20, 20"
+const STROKE_COLOR = "184, 158, 231"
 const DOT_COLOR = "8, 1, 221"
+const MOUSE_COLOR = "71, 12, 223"
 
 /**
  * No Pains No Gains
@@ -48,7 +49,7 @@ ctx.fillText(SECOND_LINE, 0, WORD_SIZE * 2)
 const imageData = ctx.getImageData(0, 0, 2000, 2000)
 const my_gradient = ctx.createLinearGradient(0, 0, 170, 0)
 my_gradient.addColorStop(0, "white")
-my_gradient.addColorStop(1, "#beec40d2")
+my_gradient.addColorStop(1, `rgb(${MOUSE_COLOR})`)
 
 class Particle {
   constructor(x, y, color, psize) {
