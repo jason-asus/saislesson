d3b87a355930e14fdb08dfb3202425c31ff5395e
blue-harry <96937529+blue-harry@users.noreply.github.com>
2024-12-03 10:33:23 +0800

    firework final version

--- src/coding/firework.js
+++ src/coding/firework.js
@@ -1,14 +1,14 @@
 /**
  *  You set your style values below
  */
-const FIREWORK_NUMBER = 5
+const FIREWORK_NUMBER = 8
 const SHOOT_LENGTH = 50
 const SPARKLE_LIFE = 300
 const SPARKLE_FLOWER = 20
 const EXPLODE_SPEED = 20
 const SPARKLE_LENGTH = 20
-const SPARKLE_HEAD = 2
-const WIND = -0.01
+const SPARKLE_HEAD = 2.5
+const WIND = -0.02
 
 /** Keep below unchanged */
 
