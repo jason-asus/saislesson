bedb2ce2a8ea8359d4de2cf80ec85af508fd29c1
jason-asus <familydiary2013@gmail.com>
2024-12-03 16:10:29 +0800

    10

--- src/coding/firework.js
+++ src/coding/firework.js
@@ -1,11 +1,11 @@
 /**
  *  You set your style values below
  */
-const FIREWORK_NUMBER = 8
+const FIREWORK_NUMBER = 10
 const SHOOT_LENGTH = 50
-const SPARKLE_LIFE = 300
+const SPARKLE_LIFE = 10
 const SPARKLE_FLOWER = 20
-const EXPLODE_SPEED = 20
+const EXPLODE_SPEED = 10
 const SPARKLE_LENGTH = 20
 const SPARKLE_HEAD = 2.5
 const WIND = -0.02
