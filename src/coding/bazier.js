0619aa4698dae3a159b21efa205dd47c879730b8
blue-harry <96937529+blue-harry@users.noreply.github.com>
2024-12-09 16:27:30 +0800

    Update bazier.js

--- src/coding/bazier.js
+++ src/coding/bazier.js
@@ -1,8 +1,8 @@
 /** settings */
 
 let COLOR = [255, 0, 0]
-let STROKE_WEIGHT = 20
-let OFFSET_INC = 10
+let STROKE_WEIGHT = 5
+let OFFSET_INC = 5
 let OFFSET_INIT = [1, 2, 3, 4, 5, 6, 7, 8]
 
 /** code */
