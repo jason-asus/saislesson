ad82fcc7bdc9dc1ba79c4c4ed149625a6f01d495
blue-harry <96937529+blue-harry@users.noreply.github.com>
2024-12-09 16:38:26 +0800

    Bazier

---
+++ src/coding/bazier.js
@@ -0,0 +1,40 @@
+/** settings */
+
+let COLOR = [255, 0, 0]
+let STROKE_WEIGHT = 5
+let OFFSET_INC = 5
+let OFFSET_INIT = [1, 2, 3, 4, 5, 6, 7, 8]
+
+/** code */
+let x1
+let x2
+let x3
+let x4
+let y1
+let y2
+let y3
+let y4
+let offset = 0
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight)
+}
+
+function draw() {
+  // background(BACKGROUND_COLOR)
+  strokeWeight(STROKE_WEIGHT / 50)
+  stroke(COLOR[0], COLOR[1], COLOR[2])
+  noFill()
+  x1 = noise(offset + OFFSET_INIT[0]) * width
+  x2 = noise(offset + OFFSET_INIT[1]) * width
+  x3 = noise(offset + OFFSET_INIT[2]) * width
+  x4 = noise(offset + OFFSET_INIT[3]) * width
+  y1 = noise(offset + OFFSET_INIT[4]) * height
+  y2 = noise(offset + OFFSET_INIT[5]) * height
+  y3 = noise(offset + OFFSET_INIT[6]) * height
+  y4 = noise(offset + OFFSET_INIT[7]) * height
+
+  offset += OFFSET_INC / 1000
+
+  bezier(x1, y1, x2, y2, x3, y3, x4, y4)
+}
