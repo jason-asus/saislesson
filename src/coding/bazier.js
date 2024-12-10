1a2e7256155b5b2c211fcfd27e9d68ee225a2f70
jason-asus <familydiary2013@gmail.com>
2024-12-10 16:18:13 +0800

    hfio.∆ vcxihikmngfgxdugmhfvxswaresthtfytyhh

--- src/coding/bazier.js
+++ src/coding/bazier.js
@@ -1,7 +1,7 @@
 /** settings */
 
-let COLOR = [255, 0, 0]
-let STROKE_WEIGHT = 5
+let COLOR = [139, 222, 153]
+let STROKE_WEIGHT = 8
 let OFFSET_INC = 5
 let OFFSET_INIT = [1, 2, 3, 4, 5, 6, 7, 8]
 
