55451bb6e1bd20f5fc392d1525918b657c2c6b79
jason-asus <familydiary2013@gmail.com>
2024-12-17 16:17:41 +0800

    merrya

--- src/coding/harry.js
+++ src/coding/harry.js
@@ -1,6 +1,6 @@
 /** settings */
-let BACKGROUND_COLOR = [200, 150, 227, 150]
-let SIZE = 8
+let BACKGROUND_COLOR = [192, 147, 228, 150]
+let SIZE = 5
 let WORDS = "MERRY CHRISTMAS AND HAPPY NEW YEAR!"
 let STROKE = true
 let GRAIN = 150
@@ -10,7 +10,7 @@ let img
 let cols, rows
 let sizes = []
 function preload() {
-  img = loadImage("harry.jpg")
+  img = loadImage("merrya.jpg")
 }
 
 function setup() {
