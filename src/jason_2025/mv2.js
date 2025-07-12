5873c5893bfd03523004852738be14e9e95f96d0
blue-harry <harryhuang2012@gmail.com>
2025-07-12 16:47:04 +0800

    Jason-2025

---
+++ src/jason_2025/mv2.js
@@ -0,0 +1,86 @@
+/** settings */
+const NUMBER_OF_SAMPLES = 256
+
+//canvas
+const canvas = document.getElementById("cnvs")
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+const ctx = canvas.getContext("2d")
+const fileload = document.getElementById("fileload")
+let audioSource
+let analyser
+
+fileload.addEventListener("change", function () {
+  const audio1 = document.getElementById("audio1")
+  audio1.src = URL.createObjectURL(this.files[0])
+  audio1.load()
+  audio1.play()
+
+  // analyze the music
+  const audioContext = new AudioContext()
+  audioSource = audioContext.createMediaElementSource(audio1)
+  analyser = audioContext.createAnalyser()
+  audioSource.connect(analyser)
+  analyser.connect(audioContext.destination)
+  analyser.fftSize = NUMBER_OF_SAMPLES
+  const bufferLenght = analyser.frequencyBinCount
+  const dataArr = new Uint8Array(bufferLenght)
+
+  const barWidth = canvas.width / bufferLenght / 2
+  let barHeight
+  let x
+
+  function animate() {
+    x = 0
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    analyser.getByteFrequencyData(dataArr) // array of values , each represents volume of the frequency
+
+    musicVisualiser(bufferLenght, x, barWidth, barHeight, dataArr) //mv
+
+    requestAnimationFrame(animate)
+  }
+  animate()
+})
+
+// Music Visualiser function
+function musicVisualiser(bufferLenght, x, barWidth, barHeight, dataArr) {
+  for (let i = 0; i < bufferLenght; i++) {
+    barHeight = dataArr[i] * 2
+
+    ctx.save()
+
+    // white block
+    ctx.fillStyle = "white"
+    ctx.fillRect(
+      canvas.width / 2 - x,
+      canvas.height - barHeight - 10,
+      barWidth,
+      10
+    )
+    ctx.fillRect(
+      canvas.width / 2 + x,
+      canvas.height - barHeight - 10,
+      barWidth,
+      10
+    )
+
+    // color bars
+    const hue = (360 * i) / bufferLenght
+    ctx.fillStyle = "hsl(" + hue + ",80%,50%)"
+    ctx.fillRect(
+      canvas.width / 2 - x,
+      canvas.height - barHeight,
+      barWidth,
+      barHeight
+    )
+    ctx.fillRect(
+      canvas.width / 2 + x,
+      canvas.height - barHeight,
+      barWidth,
+      barHeight
+    )
+
+    x += barWidth
+    ctx.restore()
+  }
+}
