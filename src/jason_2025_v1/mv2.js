5e74c7879252afbf0474021e32f8f7b5a40f60f4
NutApp <nutapp@foxmail.com>
2025-07-18 20:06:46 +0800

    for loop

--- src/jason_2025_v1/mv2.js
+++ src/jason_2025_v1/mv2.js
@@ -9,10 +9,15 @@ const ctx = canvas.getContext("2d");
 const fileload = document.getElementById("fileload");
 let audioSource;
 
-let rects = [
-  { x: 0, y: 100, vx: Math.random() * 4 + 1, vy: Math.random() * 4 + 2 },
-  { x: 0, y: 100, vx: Math.random() * 4 + 1, vy: Math.random() * 4 + 2 },
-];
+let rects = [];
+for (let i = 0; i < 11; i++) {
+  rects.push({
+    x: 0,
+    y: 100,
+    vx: Math.random() * 4 + 1,
+    vy: Math.random() * 4 + 2,
+  });
+}
 
 fileload.addEventListener("change", function () {
   const audio1 = document.getElementById("audio1");
@@ -23,14 +28,10 @@ fileload.addEventListener("change", function () {
   function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  
-
-   
-
-      for (let rect of rects) {
-    ctx.fillRect(rect.x, rect.y, 200, 200);
- rect.x += rect.vx;
-     rect.y += rect.vy;
+    for (let rect of rects) {
+      ctx.fillRect(rect.x, rect.y, 200, 200);
+      rect.x += rect.vx;
+      rect.y += rect.vy;
     }
 
     ctx.fillStyle = "red";
