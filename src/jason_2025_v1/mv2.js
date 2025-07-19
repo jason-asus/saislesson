59a0ac21a494dee51fad777c695bbf50c66b9bd9
NutApp <nutapp@foxmail.com>
2025-07-19 19:58:24 +0800

    x: i*60,

--- src/jason_2025_v1/mv2.js
+++ src/jason_2025_v1/mv2.js
@@ -8,11 +8,12 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 const fileload = document.getElementById("fileload");
 let audioSource;
+let rectSize = 50
 
 let rects = [];
 for (let i = 0; i < 11; i++) {
   rects.push({
-    x: 0,
+    x: i*60,
     y: 100,
     vx: Math.random() * 4 + 1,
     vy: Math.random() * 4 + 2,
@@ -29,12 +30,12 @@ fileload.addEventListener("change", function () {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     for (let rect of rects) {
-      ctx.fillRect(rect.x, rect.y, 200, 200);
+      ctx.fillRect(rect.x, rect.y, rectSize, rectSize);
       rect.x += rect.vx;
       rect.y += rect.vy;
     }
 
-    ctx.fillStyle = "red";
+    ctx.fillStyle = "green";
 
     requestAnimationFrame(animate);
   }
