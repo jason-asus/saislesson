8be73e2cb4bdbf6cf17e38179a3123881eed60c4
huangchaoqing <huangchaoqing@hotmail.com>
2025-07-23 16:20:59 +0800

    collision

--- src/jason_2025_v1/mv2.js
+++ src/jason_2025_v1/mv2.js
@@ -12,6 +12,7 @@ let rectSize = 50
 
 let rects = [];
 for (let i = 0; i < 11; i++) {
+
   rects.push({
     x: i * 120,
     y: 100,
@@ -34,31 +35,45 @@ fileload.addEventListener("change", function () {
   function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (let rect of rects) {
+    rects.forEach((rect, index) => {
+
+      rects.forEach((_rect, _index) => {
+        console.log(index,_index)///
+        if (index != _index) {
+          if (Math.abs(rect.x - _rect.x) <= rectSize && Math.abs(rect.y - _rect.y) < rectSize) {
+            rect.vx *= -1
+          } 
+          if (Math.abs(rect.y - _rect.y) <= rectSize && Math.abs(rect.x - _rect.x) < rectSize) {
+            rect.vy *= -1
+          }
+        }
+
+      })
+      //
+      //draw rect
       ctx.fillRect(rect.x, rect.y, rectSize, rectSize);
+      //rect vl 
       rect.x += rect.vx;
       rect.y += rect.vy;
-       // X collision
+      // X collision
       if (rect.x > canvas.width - rectSize) {
-      rect.vx = -1
-      } else if (rect.x < 0){
-        rect.vx = 1
+        rect.vx *= -1
+      } else if (rect.x < 0) {
+        rect.vx *= -1
       }
       // Y collision
-       if (rect.y > canvas.height - rectSize) {
-      rect.vy = -1
-      } else if (rect.y < 0){
-        rect.vy = 1
+      if (rect.y > canvas.height - rectSize) {
+        rect.vy *= -1
+      } else if (rect.y < 0) {
+        rect.vy *= -1
       }
-      
 
       ctx.fillStyle = `rgb(${rect.rgb.r},${rect.rgb.g},${rect.rgb.b})`;
 
-    }
+    })
 
+  requestAnimationFrame(animate);
 
-
-    requestAnimationFrame(animate);
-  }
+}
   animate();
 });
