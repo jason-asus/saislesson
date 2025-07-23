1c206557b78870d54b9819ec2bda4e768e49389a
blue-harry <harryhuang2012@gmail.com>
2025-07-23 17:07:13 +0800

    nested loop

--- src/jason_2025_v1/mv2.js
+++ src/jason_2025_v1/mv2.js
@@ -8,11 +8,10 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 const fileload = document.getElementById("fileload");
 let audioSource;
-let rectSize = 50
+let rectSize = 50;
 
 let rects = [];
 for (let i = 0; i < 11; i++) {
-
   rects.push({
     x: i * 120,
     y: 100,
@@ -35,45 +34,49 @@ fileload.addEventListener("change", function () {
   function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    rects.forEach((rect, index) => {
-
-      rects.forEach((_rect, _index) => {
-        console.log(index,_index)///
+    for (let [index, rect] of rects.entries()) {
+      // nested loop
+      for (let [_index, _rect] of rects.entries()) {
         if (index != _index) {
-          if (Math.abs(rect.x - _rect.x) <= rectSize && Math.abs(rect.y - _rect.y) < rectSize) {
-            rect.vx *= -1
-          } 
-          if (Math.abs(rect.y - _rect.y) <= rectSize && Math.abs(rect.x - _rect.x) < rectSize) {
-            rect.vy *= -1
+          // horizontal collision
+          if (
+            Math.abs(rect.x - _rect.x) <= rectSize &&
+            Math.abs(rect.y - _rect.y) < rectSize
+          ) {
+            rect.vx *= -1;
+          }
+          // vertical collision
+          if (
+            Math.abs(rect.y - _rect.y) <= rectSize &&
+            Math.abs(rect.x - _rect.x) < rectSize
+          ) {
+            rect.vy *= -1;
           }
         }
-
-      })
-      //
-      //draw rect
-      ctx.fillRect(rect.x, rect.y, rectSize, rectSize);
-      //rect vl 
-      rect.x += rect.vx;
-      rect.y += rect.vy;
-      // X collision
-      if (rect.x > canvas.width - rectSize) {
-        rect.vx *= -1
-      } else if (rect.x < 0) {
-        rect.vx *= -1
-      }
-      // Y collision
-      if (rect.y > canvas.height - rectSize) {
-        rect.vy *= -1
-      } else if (rect.y < 0) {
-        rect.vy *= -1
       }
 
       ctx.fillStyle = `rgb(${rect.rgb.r},${rect.rgb.g},${rect.rgb.b})`;
+      //draw rect
+      ctx.fillRect(rect.x, rect.y, rectSize, rectSize);
+      // move position
+      rect.x += rect.vx;
+      rect.y += rect.vy;
 
-    })
+      // X border collision
+      if (rect.x > canvas.width - rectSize) {
+        rect.vx *= -1;
+      } else if (rect.x < 0) {
+        rect.vx *= -1;
+      }
+      // Y border collision
+      if (rect.y > canvas.height - rectSize) {
+        rect.vy *= -1;
+      } else if (rect.y < 0) {
+        rect.vy *= -1;
+      }
+    }
 
-  requestAnimationFrame(animate);
-
-}
+    requestAnimationFrame(animate);
+  }
   animate();
 });
