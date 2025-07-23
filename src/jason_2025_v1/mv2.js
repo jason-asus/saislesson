635d18a1013572239abd052695f0685bc646e57f
blue-harry <harryhuang2012@gmail.com>
2025-07-23 22:07:04 +0800

    v5.7.23

--- src/jason_2025_v1/mv2.js
+++ src/jason_2025_v1/mv2.js
@@ -13,20 +13,20 @@ let rectSize = 50;
 let rects = [];
 for (let i = 0; i < 5; i++) {
   rects.push({
-    x: i * 120,
-    y: 100,
+    x: i * 200 + 10,
+    y: i * 100 + 10,
     rgb: {
       r: Math.random() * 255,
       g: Math.random() * 255,
       b: Math.random() * 255,
     },
-    vx: Math.random() * 4 + 1,
+    vx: Math.random() * 4 + 2,
     vy: Math.random() * 4 + 2,
   });
 }
 
 const addRect = () => {
-  if (rects.length < 30) {
+  if (rects.length < 50) {
     rects.push({
       x: Math.random() * canvas.width,
       y: Math.random() * canvas.height,
@@ -35,7 +35,7 @@ const addRect = () => {
         g: Math.random() * 255,
         b: Math.random() * 255,
       },
-      vx: Math.random() * 4 + 1,
+      vx: Math.random() * 4 + 2,
       vy: Math.random() * 4 + 2,
     });
   }
@@ -59,7 +59,7 @@ fileload.addEventListener("change", function () {
           // horizontal collision
           if (
             rect.x - _rect.x > 0 &&
-            rect.x - _rect.x < rectSize &&
+            rect.x - _rect.x <= rectSize &&
             Math.abs(rect.y - _rect.y) < rectSize
           ) {
             rect.vx = Math.abs(rect.vx);
@@ -69,7 +69,7 @@ fileload.addEventListener("change", function () {
           //
           if (
             rect.x - _rect.x < 0 &&
-            _rect.x - rect.x < rectSize &&
+            _rect.x - rect.x <= rectSize &&
             Math.abs(rect.y - _rect.y) < rectSize
           ) {
             rect.vx = -Math.abs(rect.vx);
@@ -79,7 +79,7 @@ fileload.addEventListener("change", function () {
           // vertical collision
           if (
             rect.y - _rect.y > 0 &&
-            rect.y - _rect.y < rectSize &&
+            rect.y - _rect.y <= rectSize &&
             Math.abs(rect.x - _rect.x) < rectSize
           ) {
             rect.vy = Math.abs(rect.vy);
@@ -88,7 +88,7 @@ fileload.addEventListener("change", function () {
           //
           if (
             rect.y - _rect.y < 0 &&
-            _rect.y - rect.y < rectSize &&
+            _rect.y - rect.y <= rectSize &&
             Math.abs(rect.x - _rect.x) < rectSize
           ) {
             rect.vy = -Math.abs(rect.vy);
@@ -98,26 +98,26 @@ fileload.addEventListener("change", function () {
       }
 
       //
+
+      // X border collision
+      if (rect.x >= canvas.width - rectSize) {
+        rect.vx *= -1;
+      } else if (rect.x <= 0) {
+        rect.vx *= -1;
+      }
+      // Y border collision
+      if (rect.y >= canvas.height - rectSize) {
+        rect.vy *= -1;
+      } else if (rect.y <= 0) {
+        rect.vy *= -1;
+      }
+      //
       ctx.fillStyle = `rgb(${rect.rgb.r},${rect.rgb.g},${rect.rgb.b})`;
       //draw rect
       ctx.fillRect(rect.x, rect.y, rectSize, rectSize);
       // move position
       rect.x += rect.vx;
       rect.y += rect.vy;
-
-      // X border collision
-      if (rect.x > canvas.width - rectSize) {
-        rect.vx *= -1;
-      } else if (rect.x < 0) {
-        rect.vx *= -1;
-      }
-      // Y border collision
-      if (rect.y > canvas.height - rectSize) {
-        rect.vy *= -1;
-      } else if (rect.y < 0) {
-        rect.vy *= -1;
-      }
-      //
     }
 
     requestAnimationFrame(animate);
